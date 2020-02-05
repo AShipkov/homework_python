201fdebe92e5b3a8c5dde084433833fe85645300
Andrey Shipkov <shipkov2007@ya.ru>
2020-02-05 11:37:45 +0300

    Домашнее задание

--- 2.py
+++ 2.py
@@ -7,5 +7,4 @@ input_time = int(input("Введите время в секундах: "))
 hours = input_time // 3600
 minutes = (input_time - hours*3600 )//60
 seconds = input_time - hours*3600 - minutes*60
-
 print(f'часов:{hours} минут: {minutes} секунд {seconds}')
