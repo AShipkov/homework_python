ee3a5b8be7647449e0fc460cce84ac0a34e28aae
Andrey Shipkov <shipkov2007@ya.ru>
2020-02-05 10:10:25 +0300

    Домашнее задание 04.02

---
+++ 4.py
@@ -0,0 +1,17 @@
+"""
+Пользователь вводит целое положительное число.
+Найдите самую большую цифру в числе. Для решения используйте цикл while и арифметические операции.
+"""
+input_number = input('Ведите целое положительное число: ')
+i = len(input_number)
+#print(i)
+a = int(input_number[i-1])
+while i> 0:
+    i = i-1
+    if a >= int(input_number [i]):
+        continue
+    else: a = int(input_number [i])
+print (a)
+
+
+
